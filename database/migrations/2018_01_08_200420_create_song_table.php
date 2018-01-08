4896a8c731140d3aeee68ecae7f98877366d7794
Jaime Moreno Cantó <jmc103@alu.ua.es>
2018-01-08 21:07:07 +0100

    Esquema BBDD Song

--- database/migrations/2018_01_08_200420_create_song_table.php
+++ database/migrations/2018_01_08_200420_create_song_table.php
@@ -16,6 +16,16 @@ class CreateSongTable extends Migration
         Schema::create('songs', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->nullable();
+            // TODO: Check this values after programming
+            $table->text('image')->nullable();
+            $table->text('audio')->nullable();
+            // /TODO
+            $table->date('released_at')->nullable();
+            $table->string('record')->nullable();
+            $table->string('p-line')->nullable();
+            $table->string('c-line')->nullable();
+            $table->text('description')->nullable();
+            $table->integer('plays')->default(0);
             $table->integer('user_id')->unsigned()->nullable();
             $table->foreign('user_id')->references('id')->on('users')
               ->onDelete('cascade');
